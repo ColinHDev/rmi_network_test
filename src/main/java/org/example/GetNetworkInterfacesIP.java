6215c1c1c3e265b6aa572e468d1a55219b2946e4
Colin <colinheidfeld@gmail.com>
2024-03-17 02:23:51 +0100

    get network interface by ip

--- src/main/java/org/example/GetNetworkInterfacesIP.java
+++ src/main/java/org/example/GetNetworkInterfacesIP.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Collections;
 
@@ -11,7 +12,11 @@ public class GetNetworkInterfacesIP {
         try {
             System.out.println(InetAddress.getLocalHost().getHostAddress());
             System.out.println(InetAddress.getLocalHost().getHostName());
+            System.out.println(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getDisplayName());
+            System.out.println(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getName());
         } catch (UnknownHostException e) {
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
         }
         try {
             Collections.list(NetworkInterface.getNetworkInterfaces()).forEach(netInterface -> {
